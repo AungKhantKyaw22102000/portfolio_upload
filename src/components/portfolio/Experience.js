f37b8259193920c99c724062b367ecf8534b34b2
AungKhantKyaw22102000 <akhant057@gmail.com>
2023-01-02 15:11:38 +0630

    portfolio first update

--- src/components/portfolio/Experience.js
+++ src/components/portfolio/Experience.js
@@ -6,9 +6,9 @@ export default function Experience() {
         <div className="exp">
             <h1 className="exp-head">Experience</h1>
             <ul className="exp-body">
-                <li className="exp-p">B2HD Ecomputer, Gaming PC and High End Workstation (10 Monthes)</li>
+                <li className="exp-p">B2HD Computer, Gaming PC and High End Workstation (10 Months)</li>
             </ul>
-            <p className="target">[Specialized field: I worked as the Web Developer mainly for data entry for website and design the datas.]</p>
+            <p className="target">[Specialized field: I worked as the Web Developer mainly for data entry for website and design the data.]</p>
         </div>
     )
 }
